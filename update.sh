cb0db5dc87b115cacf8c14b484a71cb18c0ff636
Johannes Zellner <johannes@cloudron.io>
2018-08-07 17:01:40 +0200

    Update mkdocs-material

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ echo "Installing mkdocs"
 sudo pip install mkdocs==0.17.1
 
 echo "Installing mkdocs-material"
-sudo pip install mkdocs-material==2.7.1
+sudo pip install mkdocs-material==3.0.3
 
 echo "Done."
