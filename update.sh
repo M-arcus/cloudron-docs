a06174d1e6ad7c2c34993c57eb5632a2cf1babce
Johannes Zellner <johannes@cloudron.io>
2019-03-19 16:00:30 -0700

    Fix mkdocs

--- update.sh
+++ update.sh
@@ -3,9 +3,12 @@
 set -eu
 
 echo "Installing mkdocs"
-sudo pip install mkdocs==0.17.1
+sudo pip install mkdocs==1.0.4
 
 echo "Installing mkdocs-material"
 sudo pip install mkdocs-material==3.0.3
+
+echo "Installing pygments"
+sudo pip install pygments
 
 echo "Done."
